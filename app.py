79e460ed472211141a82f7fc3a04e9c9b0397088
DevGevz <141859684+DevGevz@users.noreply.github.com>
2025-08-27 22:52:23 +0200

    Update app.py

--- app.py
+++ app.py
@@ -163,4 +163,6 @@ def delete_account():
 
 # -------------------- Run --------------------
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # fallback 5000 si local
+    app.run(host="0.0.0.0", port=port)
+
